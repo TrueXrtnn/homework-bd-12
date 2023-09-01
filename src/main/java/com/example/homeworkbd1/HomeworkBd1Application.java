c70445a57dd45b6fbf08884822d685f49cd6834e
Сотрудник <mr.chareev@mail.ru>
2023-09-01 14:40:13 +0800

    1

--- src/main/java/com/example/homeworkbd1/HomeworkBd1Application.java
+++ src/main/java/com/example/homeworkbd1/HomeworkBd1Application.java
@@ -1,11 +1,12 @@
 package com.example.homeworkbd1;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition
 public class HomeworkBd1Application {
-
     public static void main(String[] args) {
         SpringApplication.run(HomeworkBd1Application.class, args);
     }
