c54f3b72ede62fc5fef930c1b31fe996c6a86f65
Сотрудник <mr.chareev@mail.ru>
2023-09-01 11:22:59 +0800

    1

---
+++ src/main/java/com/example/homeworkbd1/HomeworkBd1Application.java
@@ -0,0 +1,13 @@
+package com.example.homeworkbd1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkBd1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeworkBd1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/homeworkbd1/HomeworkBd1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homeworkbd1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeworkBd1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
